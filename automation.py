7e82d49f2ed22daaaf147a96c94c162c0715e28d
Joao_Guilherme2 <80895578+JoaoG23@users.noreply.github.com>
2024-11-01 18:25:43 -0300

    :wrench: chore: Add .dev

--- automation.py
+++ automation.py
@@ -1,5 +1,6 @@
 from time import sleep
 
+
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from webdriver_manager.chrome import ChromeDriverManager
@@ -7,47 +8,29 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
 
+
 from utils.convert_format_date_to_datebr import convert_format_date_to_datebr
 
-
-service = Service(ChromeDriverManager().install())
 
 #Obs: 
 # Para não dar erro na automação, busque por um item no site
 # que aparece somente existe quando a tela estiver carregada.
 
 class Automation:
-        
+    def __init__(self, driver):
+        self.driver = driver
     def insert_items(self, items):
-        driver = webdriver.Chrome(service=service)
         try:
-            driver.get("http://localhost:81")
-            sleep(2)
-            
-            login = driver.find_element('xpath', '//*[@id="root"]/div/div[2]/main/main/div/form/div[1]/input')
-            login.send_keys("admin")
-            
-            password = driver.find_element('xpath', '//*[@id="root"]/div/div[2]/main/main/div/form/div[2]/input')
-            password.send_keys("admin")
-       
-            submit = driver.find_element('xpath', '//*[@id="root"]/div/div[2]/main/main/div/form/button')
-            submit.click()
-            sleep(3)
-            
-            ## Cadastro de tarefas
-            fluxo_caixa_button = driver.find_element('xpath', '//*[@id="root"]/div/div[2]/main/div[1]/details[1]/summary')
-            fluxo_caixa_button.click()
-            
             sleep(1)
-            todos_items_mes_button = driver.find_element('xpath', '//*[@id="root"]/div/div[2]/main/div[1]/details[1]/ul/li[1]/a')
+            todos_items_mes_button = self.driver.find_element('xpath', '//*[@id="root"]/div/div[2]/main/div[1]/details[1]/ul/li[1]/a')
             todos_items_mes_button.click()
             
             sleep(2)
-            adicionar_button = driver.find_element('xpath', '//*[@id="root"]/div/div[2]/main/div[4]/main/header/header/button')
+            adicionar_button = self.driver.find_element('xpath', '//*[@id="root"]/div/div[2]/main/div[4]/main/header/header/button')
             adicionar_button.click()
             
             sleep(1)
-            data_insercao_input = driver.find_element('xpath', '//*[@id="root"]/div/div[2]/main/div[4]/main/form/section/section[1]/main/div[1]/div/input')
+            data_insercao_input = self.driver.find_element('xpath', '//*[@id="root"]/div/div[2]/main/div[4]/main/form/section/section[1]/main/div[1]/div/input')
             
             data_insercao_value = convert_format_date_to_datebr(items[0])
             
@@ -57,35 +40,35 @@ class Automation:
             data_insercao_input.send_keys("15:30")
         
             sleep(1)
-            elemento_select_find = driver.find_element('xpath', '//*[@id="root"]/div/div[2]/main/div[4]/main/form/section/section[1]/main/div[2]/label/select')
+            elemento_select_find = self.driver.find_element('xpath', '//*[@id="root"]/div/div[2]/main/div[4]/main/form/section/section[1]/main/div[2]/label/select')
             
             elemento_select = Select(elemento_select_find)
             elemento_select.select_by_visible_text(items[1])
-            sleep(1)
-            subelemento_select_find = driver.find_element('xpath', '//*[@id="root"]/div/div[2]/main/div[4]/main/form/section/section[1]/main/div[3]/div/select')
-            
+            sleep(2)
+            subelemento_select_find = self.driver.find_element('xpath', '//*[@id="root"]/div/div[2]/main/div[4]/main/form/section/section[1]/main/div[3]/div/select')
+            sleep(2)
             subelemento_select = Select(subelemento_select_find)
             subelemento_select.select_by_visible_text(items[2])
-                
-            valor_input = driver.find_element('xpath', '//*[@id="root"]/div/div[2]/main/div[4]/main/form/section/section[1]/main/div[8]/div/input')
+            sleep(2)    
+            valor_input = self.driver.find_element('xpath', '//*[@id="root"]/div/div[2]/main/div[4]/main/form/section/section[1]/main/div[8]/div/input')
             valor_input.send_keys(items[7])
             
-            
-            entrada_saida_input = driver.find_element('xpath', '//*[@id="root"]/div/div[2]/main/div[4]/main/form/section/section[2]/div[1]/div/input')
+            sleep(2)
+            entrada_saida_input = self.driver.find_element('xpath', '//*[@id="root"]/div/div[2]/main/div[4]/main/form/section/section[2]/div[1]/div/input')
             entrada_saida_input.click()
             
-            observacao_input = driver.find_element('xpath', '//*[@id="root"]/div/div[2]/main/div[4]/main/form/section/section[3]/div/div/input')
-            observacao_input.send_keys("Criado por automacao python")
+            descricao_items = items[9]
+            observacao_input = self.driver.find_element('xpath', '//*[@id="root"]/div/div[2]/main/div[4]/main/form/section/section[3]/div/div/input')
+            observacao_input.send_keys(f"{descricao_items} by bot")
             
-            salvar_button = driver.find_element('xpath', '//*[@id="root"]/div/div[2]/main/div[4]/main/form/section/section[4]/button')
+            salvar_button = self.driver.find_element('xpath', '//*[@id="root"]/div/div[2]/main/div[4]/main/form/section/section[4]/button')
             salvar_button.click()
             
                     
             sleep(2)
         except WebDriverException as e:
             print(f"Ocorreu um erro: {e}")
-        finally:
-            driver.quit()
+            self.driver.quit()
 
         
     
--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import csv
 import os
 from dotenv import load_dotenv
+load_dotenv()
 
 from automation import Automation
 
-load_dotenv()
+from time import sleep
 
-user = os.getenv("USERNAME")
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
+
+
+username = os.getenv("USERNAME")
 password = os.getenv("PASSWORD")
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
@@ -17,9 +26,36 @@ error_excel_file_path = os.path.join(base_dir, 'files', 'errors', 'task.xlsx')
 from utils.move_to_file import move_to_file
 from utils.convert_excel_to_csv import convert_excel_to_csv
 
+service = Service(ChromeDriverManager().install())
+
 if __name__ == '__main__':
     try:
-        automation = Automation()
+        
+        driver = webdriver.Chrome(service=service)
+        driver.maximize_window()
+        driver.delete_all_cookies()
+        
+        driver.get("http://localhost:81")
+        sleep(4)
+            
+        login_input = driver.find_element('xpath', '//*[@id="root"]/div/div[2]/main/main/div/form/div[1]/input')
+        sleep(1)
+        login_input.send_keys(username)
+        
+        sleep(2)
+        password_input = driver.find_element('xpath', '//*[@id="root"]/div/div[2]/main/main/div/form/div[2]/input')
+        password_input.send_keys(password)
+        
+        sleep(2)
+        submit_button = driver.find_element('xpath', '//*[@id="root"]/div/div[2]/main/main/div/form/button')
+        submit_button.click()
+        
+        sleep(2)
+        fluxo_caixa_button = driver.find_element('xpath', '//*[@id="root"]/div/div[2]/main/div[1]/details[1]/summary')
+        fluxo_caixa_button.click()
+        
+        automation = Automation(driver)
+        
         
         convert_excel_to_csv()
         
@@ -40,4 +76,6 @@ if __name__ == '__main__':
         # Move file with dir(erros)
         move_to_file(source_excel_file_path, error_excel_file_path)
         print(f"An error occurred: {e}")
+    finally:
+        driver.quit()
     
